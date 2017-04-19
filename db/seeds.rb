c7125001655ac48196d5918760f6c3579bf0d2c1
Payal Amarnani <payalamarnani93@gmail.com>
2017-04-19 20:33:03 +0800

    db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Product.create(name:"Skincare",price:"33.50", description:"The perfect product for sensitive skin")
+Product.create(name:"Lipstick",price:"10.50", description:"Burberry Shade. Long Lasting All time favourite")
+Product.create(name:"Foundation",price:"120.00", description:"Blemish Free Long Lasting Foundation")
+Product.create(name:"Concealer",price:"80.00", description:"The perfect concealer for all ocassions")
+Product.create(name:"Highlighter",price:"80.00", description:"The perfect glow")
+Product.create(name:"Blush",price:"40.00", description:"Add colour to your face")
+Product.create(name:"Contour",price:"40.00", description:"Accentuate your features. Let your best features shine")
+Product.create(name:"Eye Shadow Palette",price:"35.00", description:"The perfect palette with over 80 shades")
+Product.create(name:"Eye Liner",price:"60.00", description:"Black Waterproof Eyeliner")
+Product.create(name:"Mascara",price:"75.00", description:"Thick Lushful Eye Lashes")
+Product.create(name:"Face Primer",price:"32.00", description:"The perfect pre-party base")
+Product.create(name:"Organiser",price:"50.50", description:"The perfect makeup organiser. Spacious and portable")
+Product.create(name:"Makeup Remover",price:"49.99", description:"The perfect makeup remover.")
+
+
+
+
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -4,4 +4,10 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+
+test "should not save product without title" do
+  product=Product.new
+  assert_not product.save, "Saved Product without title"
+end
 end
