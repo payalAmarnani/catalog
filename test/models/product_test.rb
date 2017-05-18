ce248b31abbdc0c57c2328b88a39fabfe3928eef
Payal Amarnani <payalamarnani93@gmail.com>
2017-05-18 11:03:20 +0800

    Deploy to heroic

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -10,4 +10,12 @@ test "should not save product without title" do
   product=Product.new
   assert_not product.save, "Saved Product without title"
 end
+
+test "validate product name, description" do
+	 @product=Product.new
+	 # @product.name="Abs"
+	 @product.save
+	 @product.valid?
+	assert_equal ["can't be blank"], @product.errors.messages[:name]
+end
 end
